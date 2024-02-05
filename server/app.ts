d337952219a0ff43afdcc9cd8314f4497a17e109
Mansi072003 <mansiagc36@gmail.com>
2024-02-05 15:16:52 +0530

    .

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,7 @@
 import express from "express"
 export const app=express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser
+app.use(express.json({limit: "50mb"}));//importantfor cloudinary
